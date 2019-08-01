2efe59b652792754c76954a98e6adf864d9fc79c
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-08-01 12:04:33 +0200

    installed axios and replaces fetch with axios call

--- client/src/Views/Cities.js
+++ client/src/Views/Cities.js
@@ -1,44 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 // import { set } from 'mongoose';
 
 
-
-
 class Cities extends Component { 
-
-  state={
-    cities:[]
+  state = {
+    cities: []
   }
+
   componentDidMount() {
-
-
-// or do the fetch here?
-
-// fetch API is a promise-based API returning a response object.
-// to get actual JSON content, invoke json() method of response object
-
-
-        fetch("/api/cities")
-        .then(response => response.json())
-        .then(result => this.setState({cities:result}))
-        
-        .catch(e => console.log(e));
-    
+    axios.get("/api/cities")
+      .then(res => {
+        const cities = res.data;
+        this.setState({ cities })
+      })
   }
 
   render () {
     console.log("Hola", this.state.cities);
-    
   // displays a list of countries/cities as a bullet list:
     const cities = this.state.cities.map(city => 
-      <li key={city._id }>{city.country}: {city.city}</li>) 
+      <li key = { city._id } > { city.country }: { city.city } </li>
+      ) 
   
     return (
       <div>
-          <h1>Cities</h1>
+        <h1>Cities</h1>
          <ul> {cities}</ul>
-         <ul>{this.state.cities.map(city => 
-      <li key={city._id }>{city.country}: {city.city}</li>) }</ul>
       </div>
     )
   };  
@@ -55,23 +43,4 @@ class Cities extends Component {
 // })
 
 
-
 export default  Cities
-
-
-// ********** Resource: https://reactjs.org/docs/lists-and-keys.html **********
-// function fetchCities(props) {
-//   const cities = props.cities;
-//   const listItems = cities.map((city) =>
-//     <li key= {} >{city}</li>
-//   );
-//   return (
-//     <ul>{listItems}</ul>
-//   );
-// }
-
-// const cities = [1, 2, 3, 4, 5];
-// ReactDOM.render(
-//   <NumberList numbers={cities} />,
-//   document.getElementById('root')
-// );

e9ed5bc1e7088df45a29002d52e5fefe7492e796
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-08-25 18:04:04 +0200

    reworked the redirect functions on Home.js

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -1,23 +1,50 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
 
-// import styles
+// import components & styles
+import BackHome from '../components/BackHome'
 import './views.css';
 import logo from '../images/MYtineraryLogo.png';
 import arrow from '../images/circled-right-2.png';
 
 class Home extends React.Component {
     state = {
-      redirect: false
+      redirectLogin: false,
+      redirectAccount: false,
+      redirectCities: false
     }
-    setRedirect = () => {
-      this.setState({ redirect: true })
+
+    // Redirect for Login page
+    setRedirectLogin = () => {
+      this.setState({ redirectLogin: true })
     }
-    renderRedirect = () => {
-      if (this.state.redirect) {
+    renderRedirectLogin = () => {
+      if (this.state.redirectLogin) {
         return <Redirect to='/login' />
       }
     }
+
+    // Redirect for Account page
+    setRedirectAccount = () => {
+      this.setState({ redirectAccount: true })
+      }
+    renderRedirectAccount = () => {
+      if (this.state.redirectAccount) {
+        return <Redirect to='/createaccount' />
+      }
+    }
+
+    // Redirect for Cities page
+    // {/* TODO: redirect to itineraties */}
+    setRedirectCities = () => {
+      this.setState({ redirectCities: true })
+      }
+    renderRedirectCities = () => {
+      if (this.state.redirectCities) {
+        return <Redirect to='/cities' />
+      }
+    }
+
     render () {
       return (
           <div className="logo">
@@ -26,15 +53,32 @@ class Home extends React.Component {
             <h2>Start browsing</h2>
             
             <div>
-              {this.renderRedirect()}
-              <button onClick={this.setRedirect}><img className="arrow" src={arrow}  alt="arrow" />
-              </button>
+              { this.renderRedirectCities() }
+                  <button 
+                    onClick={ this.setRedirectCities }>
+                      <img className="arrow" src={arrow}  alt="arrow" />
+                  </button>
             </div>
             
-            
             <p className="bold">Want to build your own itinerary?</p>
-            <div className="links"></div>
+            <div className="links">
+              { this.renderRedirectLogin() }
+                  <button onClick={ this.setRedirectLogin }>Login
+                  </button>
+
+              { this.renderRedirectAccount() }
+                  <button onClick={ this.setRedirectAccount }>Create Account
+                  </button>
+
+            </div>
+          
+            {/* home to disable to the home button on the home page? */}
+            <div className="home-at-home">
+            <BackHome />
+            </div>
+
           </div>
+         
       )
     }
 }

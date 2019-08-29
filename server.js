22ada85b2cbd857189cc37e910eef067128e9735
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-08-29 11:24:10 +0200

    revised package.json and specified node and npm versions

--- server.js
+++ server.js
@@ -31,15 +31,15 @@ app.use('/api/cities', cities)
 app.use('/api/itineraries', itineraries)
 app.use('/api/activities', activities)
 
-// serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-// set static folder
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 
 const port = process.env.PORT || 5000;
